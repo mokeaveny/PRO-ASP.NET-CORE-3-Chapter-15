ad57558c28ab70d4101137bae68919e515d73a81
Michael Keaveny <michaelkeaveny0@gmail.com>
2021-05-17 17:56:59 +0100

    Changing the configuration data and environment variables

--- Startup.cs
+++ Startup.cs
@@ -8,20 +8,41 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
 
 namespace ASP.NET_CORE_3_Chapter_15
 {
     public class Startup
     {
+        public Startup(IConfiguration configService)
+        {
+            Configuration = configService;
+        }
+
+        private IConfiguration Configuration { get; set; }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MessageOptions>(Configuration.GetSection("Location"));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseRouting();
+
+            app.UseMiddleware<LocationMiddleware>();
+
+            app.Use(async (context, next) =>
+            {
+                string defaultDebug = Configuration["Logging:LogLevel:Default"];
+                await context.Response
+                    .WriteAsync($"The config setting is: {defaultDebug}");
+                string environ = Configuration["ASPNETCORE_ENVIRONMENT"];
+                await context.Response
+                    .WriteAsync($"\nThe env setting is: {environ}");
+            });
+
             app.UseEndpoints(endpoints => {
                 endpoints.MapGet("/", async context => {
                     await context.Response.WriteAsync("Hello World!");
