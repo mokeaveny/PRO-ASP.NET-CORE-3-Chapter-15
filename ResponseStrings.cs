7ddd7880eb9f4085adbb613562a14e31f687f5fe
Michael Keaveny <michaelkeaveny0@gmail.com>
2021-05-18 19:37:13 +0100

    Completed chapter 16, added HTTPS for more security reasons. As well as error handling and exceptions

---
+++ ResponseStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_3_Chapter_15
+{
+    public class Responses
+    {
+        public static string DefaultResponse = @"
+        <!DOCTYPE html>
+            <html lang=""en"">
+                <head>
+                    <link rel=""stylesheet""
+                      href=""/lib/twitter-bootstrap/css/bootstrap.min.css"" />
+                    <title>Error</title>
+                </head>
+                <body class=""text-center"">
+                    <h3 class=""p-2"">Error {0}</h3>
+                    <h6>
+                        You can go back to the <a href=""/"">homepage</a> and try again
+                    </h6>
+                </body>
+            </html>";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.HostFiltering;
 
 namespace ASP.NET_CORE_3_Chapter_15
 {
@@ -17,34 +19,72 @@ namespace ASP.NET_CORE_3_Chapter_15
             {
                 opts.CheckConsentNeeded = context => true;
             });
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.IsEssential = true;
+            });
+
+            services.AddHsts(opts =>
+            {
+                opts.MaxAge = TimeSpan.FromDays(1);
+                opts.IncludeSubDomains = true;
+            });
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
+            //app.UseDeveloperExceptionPage();
+            app.UseExceptionHandler("/error.html");
+            if (env.IsProduction())
+            {
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
             app.UseCookiePolicy();
+            app.UseStaticFiles();
+            app.UseStatusCodePages("text/html", Responses.DefaultResponse);
             app.UseMiddleware<ConsentMiddleware>();
+            app.UseSession();
             app.UseRouting();
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/error")
+                {
+                    context.Response.StatusCode = StatusCodes.Status404NotFound;
+                    await Task.CompletedTask;
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
+            app.Run(context =>
+            {
+                throw new Exception("Something has gone wrong");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response
+                    .WriteAsync($"HTTPS Request: {context.Request.IsHttps} \n");
+                await next();
+            });
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/cookie", async context =>
                 {
-                    int counter1 =
-                        int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter1", counter1.ToString(),
-                        new CookieOptions
-                        {
-                            MaxAge = TimeSpan.FromMinutes(30),
-                            IsEssential = true
-                        });
-                    int counter2 =
-                        int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter2", counter2.ToString(),
-                        new CookieOptions
-                        {
-                            MaxAge = TimeSpan.FromMinutes(30)
-                        });
+                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+                    context.Session.SetInt32("counter1", counter1);
+                    context.Session.SetInt32("counter2", counter2);
+                    await context.Session.CommitAsync();
                     await context.Response
                         .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
                 });
