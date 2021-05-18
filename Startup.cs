6428218834250a5686144670a496123069fbcd16
Michael Keaveny <michaelkeaveny0@gmail.com>
2021-05-18 20:56:30 +0100

    Utilising the cache service. Created an endpoint that performs a large calculation, which is then cached

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.HostFiltering;
 
 namespace ASP.NET_CORE_3_Chapter_15
 {
@@ -15,92 +8,26 @@ namespace ASP.NET_CORE_3_Chapter_15
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(opts =>
+            services.AddDistributedMemoryCache(opts =>
             {
-                opts.CheckConsentNeeded = context => true;
-            });
-
-            services.AddDistributedMemoryCache();
-
-            services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromMinutes(30);
-                options.Cookie.IsEssential = true;
-            });
-
-            services.AddHsts(opts =>
-            {
-                opts.MaxAge = TimeSpan.FromDays(1);
-                opts.IncludeSubDomains = true;
+                opts.SizeLimit = 200;
             });
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            //app.UseDeveloperExceptionPage();
-            app.UseExceptionHandler("/error.html");
-            if (env.IsProduction())
-            {
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseCookiePolicy();
+            app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
-            app.UseStatusCodePages("text/html", Responses.DefaultResponse);
-            app.UseMiddleware<ConsentMiddleware>();
-            app.UseSession();
             app.UseRouting();
-
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path == "/error")
-                {
-                    context.Response.StatusCode = StatusCodes.Status404NotFound;
-                    await Task.CompletedTask;
-                }
-                else
-                {
-                    await next();
-                }
-            });
-
-            app.Run(context =>
-            {
-                throw new Exception("Something has gone wrong");
-            });
-
-            app.Use(async (context, next) =>
-            {
-                await context.Response
-                    .WriteAsync($"HTTPS Request: {context.Request.IsHttps} \n");
-                await next();
-            });
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/cookie", async context =>
-                {
-                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
-                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
-                    context.Session.SetInt32("counter1", counter1);
-                    context.Session.SetInt32("counter2", counter2);
-                    await context.Session.CommitAsync();
-                    await context.Response
-                        .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-                });
+                endpoints.MapEndpoint<SumEndpoint>("/sum/{count:int=1000000000}");
 
-                endpoints.MapGet("clear", context =>
+                endpoints.MapGet("/", async context =>
                 {
-                    context.Response.Cookies.Delete("counter1");
-                    context.Response.Cookies.Delete("counter2");
-                    context.Response.Redirect("/");
-                    return Task.CompletedTask;
+                    await context.Response.WriteAsync("Hello World!");
                 });
-
-                endpoints.MapFallback(async context =>
-                    await context.Response.WriteAsync("Hello World!"));
             });
         }
-
     }
 }
---
+++ SumEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace ASP.NET_CORE_3_Chapter_15
+{
+    public class SumEndpoint
+    {
+        public async Task Endpoint(HttpContext context, IDistributedCache cache)
+        {
+            int count = int.Parse((string)context.Request.RouteValues["count"]);
+            string cacheKey = $"sum_{count}";
+            string totalString = await cache.GetStringAsync(cacheKey);
+            if (totalString == null)
+            {
+                long total = 0;
+                for (int i = 1; i <= count; i++)
+                {
+                    total += 1;
+                }
+                totalString = $"({ DateTime.Now.ToLongTimeString() }) {total}";
+                await cache.SetStringAsync(cacheKey, totalString,
+                    new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(2)
+                    });
+            }
+            await context.Response.WriteAsync(
+                $"({DateTime.Now.ToLongTimeString()}) Total for {count}"
+                + $" values:\n{totalString}\n");
+        }
+    }
+}
