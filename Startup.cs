568ed574975ee95f1a28d928c0435d005717380b
Michael Keaveny <michaelkeaveny0@gmail.com>
2021-05-20 10:46:31 +0100

    Refactored the project to cache entire responses instead of individual data items using an SqlServerCache method

--- Startup.cs
+++ Startup.cs
@@ -1,22 +1,37 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using ASP.NET_CORE_3_Chapter_15.Services;
 
 namespace ASP.NET_CORE_3_Chapter_15
 {
     public class Startup
     {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        private IConfiguration Configuration { get; set; }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDistributedMemoryCache(opts =>
+            services.AddDistributedSqlServerCache(opts =>
             {
-                opts.SizeLimit = 200;
+                opts.ConnectionString
+                    = Configuration["ConnectionStrings:CacheConnection"];
+                opts.SchemaName = "dbo";
+                opts.TableName = "DataCache";
             });
+            services.AddResponseCaching();
+            services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
         }
 
         public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
+            app.UseResponseCaching();
             app.UseStaticFiles();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
--- SumEndpoint.cs
+++ SumEndpoint.cs
@@ -2,33 +2,33 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.AspNetCore.Routing;
+using ASP.NET_CORE_3_Chapter_15.Services;
 
 namespace ASP.NET_CORE_3_Chapter_15
 {
     public class SumEndpoint
     {
-        public async Task Endpoint(HttpContext context, IDistributedCache cache)
+        public async Task Endpoint(HttpContext context, IDistributedCache cache,
+            IResponseFormatter formatter, LinkGenerator generator)
         {
             int count = int.Parse((string)context.Request.RouteValues["count"]);
-            string cacheKey = $"sum_{count}";
-            string totalString = await cache.GetStringAsync(cacheKey);
-            if (totalString == null)
+            long total = 0;
+            for (int i = 1; i <= count; i++)
             {
-                long total = 0;
-                for (int i = 1; i <= count; i++)
-                {
-                    total += 1;
-                }
-                totalString = $"({ DateTime.Now.ToLongTimeString() }) {total}";
-                await cache.SetStringAsync(cacheKey, totalString,
-                    new DistributedCacheEntryOptions
-                    {
-                        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(2)
-                    });
+                total += i;
             }
-            await context.Response.WriteAsync(
-                $"({DateTime.Now.ToLongTimeString()}) Total for {count}"
-                + $" values:\n{totalString}\n");
+            string totalString = $"({ DateTime.Now.ToLongTimeString() }) {total}";
+
+            context.Response.Headers["Cache-Control"] = "public, max-age=120";
+
+            string url = generator.GetPathByRouteValues(context, null,
+                new { count = count });
+
+            await formatter.Format(context,
+                $"<div>({DateTime.Now.ToLongTimeString()}) Total for {count}"
+                + $" values:</div><div>{totalString}</div>"
+                + $"<a href={url}>Reload</a>");
         }
     }
 }
